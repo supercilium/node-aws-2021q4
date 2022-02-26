48e4ef6970a078fac7c5788e0d0ea35fe4ac0679
Supercilium <marusel.12@gmail.com>
2022-02-26 15:27:11 +0300

    add proxy

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -4,7 +4,7 @@ const API_PATHS = {
   order: 'https://d34wlkqhjh.execute-api.eu-west-1.amazonaws.com/dev',
   import: 'https://8soonfz4j0.execute-api.eu-west-1.amazonaws.com/dev',
   bff: 'https://d34wlkqhjh.execute-api.eu-west-1.amazonaws.com/dev',
-  cart: 'http://supercilium-cart-api-dev1.eu-west-1.elasticbeanstalk.com/api',
+  cart: 'https://jriku0lj72.execute-api.eu-west-1.amazonaws.com/dev',
 };
 
 export default API_PATHS;
